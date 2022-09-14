d92913ff5b994383846958d3d1cb2c3901beb1b1
Fernando Machado <fernandomachadoweb@gmail.com>
2022-09-14 10:24:46 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // coloque a nota para teste
+        int nota = 80;
+        String graduacao;
+
+        // A 80, B 70, C 60, D 0
+        // função if-else
+
+
+        if (nota >= 80) {
+            graduacao = "A";
+        } else if (nota < 80 && nota >= 70) {
+            graduacao = "B";
+        } else if (nota < 70 && nota >= 60) {
+            graduacao = "C";
+        } else if (nota < 60 && nota >= 0) {
+            graduacao = "D";
+        } else {
+            graduacao = "";
+        }
+
+		// função switch / case	
+
+        switch (graduacao) {
+            case "A":
+            case "B":
+                System.out.println("Aluno Aprovado!");
+                break;
+            case "C":
+            case "D":
+                System.out.println("Aluno Reprovado!");
+                break;
+            default:
+                System.out.println("Graduação Inválida");
+
+        }
+
+    }
+
+}
